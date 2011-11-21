f8e0a7f109d4445fecc243ded7c0b8588736ad8c
lzdych <lukas.zdych@gmail.com>
2011-11-21 14:09:26 +0100

    added basic tests inspired by tests in plone.app.page

--- plone/app/layoutpage/testing.py
+++ plone/app/layoutpage/testing.py
@@ -3,20 +3,30 @@ from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import FunctionalTesting
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
 
 
-class Layer(PloneSandboxLayer):
-    
-    defaultBases = (PLONE_FIXTURE,)
-    
+class LayoutPageLayer(PloneSandboxLayer):
+
+    defaultBases = PLONE_FIXTURE,
+
     def setUpZope(self, app, configurationContext):
         import plone.app.layoutpage
+        #import plone.app.toolbar
+        #self.loadZCML(package=plone.app.toolbar) # FIXME: this should be fixed in deco?
         self.loadZCML(package=plone.app.layoutpage)
 
     def setUpPloneSite(self, portal):
         self.applyProfile(portal, 'plone.app.layoutpage:default')
 
+        setRoles(portal, TEST_USER_ID, ['Manager', 'Member'])
+        self['folder'] = portal[portal.invokeFactory(type_name='Folder',
+                                id='foo-folder', title='Foo Folder')]
+        setRoles(portal, TEST_USER_ID, ['Member'])
 
-FIXTURE = Layer()
-INTEGRATION_TESTING = IntegrationTesting(bases=(FIXTURE,), name='plone.app.layoutpage:Integration')
-FUNCTIONAL_TESTING = FunctionalTesting(bases=(FIXTURE,), name='plone.app.layoutpage:Functional')
+LAYOUT_PAGE_FIXTURE = LayoutPageLayer()
+LAYOUT_PAGE_INTEGRATION_TESTING = IntegrationTesting(bases=(LAYOUT_PAGE_FIXTURE, ),
+        name='LayoutPage:Integration')
+LAYOUT_PAGE_FUNCTIONAL_TESTING = FunctionalTesting(bases=(LAYOUT_PAGE_FIXTURE, ),
+                                       name='LayoutPage:Functional')
