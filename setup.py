ec360392650bf88d576336a500f50e564f7acc87
Rok Garbas <rok@garbas.si>
2012-06-23 07:58:36 +0200

    update dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,46 @@
 from setuptools import setup, find_packages
 
-version = '1.0b1'
+version = '1.0'
+tests_require = [
+    'plone.testing',
+    'plone.app.testing',
+    'plone.behavior',
+    ]
 
-setup(name='plone.app.layoutpage',
-      version=version,
-      description="A basic page content type for Plone using Deco layout",
-      long_description=open("README.rst").read() + "\n" +
-                       open("CHANGES.txt").read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='plone.app.layoutpage',
+    version=version,
+    description="A basic page content type for Plone using Deco layout",
+    long_description=open("README.rst").read(),
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
-      author='David Glick',
-      author_email='dglick@gmail.com',
-      url='http://github.com/plone/plone.app.layoutpage',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.app.blocks',
-          'plone.app.dexterity',
-          # -*- Extra requirements: -*-
-      ],
-      extras_require={
-        'test': ['plone.app.testing'],
-      },      
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    keywords='',
+    author='David Glick',
+    author_email='dglick@gmail.com',
+    url='http://github.com/plone/plone.app.layoutpage',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.component',
+        'zope.lifecycleevent',
+        'z3c.form',
+        'Zope2',
+        'plone.dexterity',
+        'plone.app.blocks',
+        'plone.app.dexterity',
+    ],
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    entry_points="""
+        [z3c.autoinclude.plugin]
+        target = plone
+        """,
+    )
